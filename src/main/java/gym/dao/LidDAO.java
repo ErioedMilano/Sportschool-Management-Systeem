3e30d8dd2476d2fe3950b60d5a70181a29aae316
ErioedMilano <erioedjr@hotmail.com>
2025-03-21 23:33:42 -0300

    update addLID() methode in LidDAO

--- src/main/java/gym/dao/LidDAO.java
+++ src/main/java/gym/dao/LidDAO.java
@@ -1,4 +1,33 @@
 package gym.dao;
 
+import gym.DatabaseConnection;
+import gym.models.Lid;
+
+import java.sql.*;
+
 public class LidDAO {
+
+    //Create
+    public void addLid(Lid lid) throws SQLException {
+
+
+        String sql = "INSERT INTO leden (naam, email, geboortedatum) VALEUS (?, ?,?)" ;
+
+        try (Connection connection = DatabaseConnection.getConnection();
+             PreparedStatement preparedStatement = connection.prepareStatement(sql, Statement.RETURN_GENERATED_KEYS)){
+
+            preparedStatement.setString(1,lid.getNaam());
+            preparedStatement.setString(2,lid.getEmail());
+            preparedStatement.setDate(3, Date.valueOf(lid.getGeboortedatum()));
+
+            preparedStatement.executeUpdate();
+
+            try (ResultSet resultSet = preparedStatement.getGeneratedKeys()){
+                if (resultSet.next()){
+                    lid.setId(resultSet.getInt(1));
+                }
+            }
+        }
+
+    }
 }
